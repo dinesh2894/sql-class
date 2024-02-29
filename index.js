cf20531e4e7d8d9f1b11c9eee25a48c56dea4232
Dinesh Saini <sainidinesh2894@gmail.com>
2024-02-29 20:43:09 +0530

    Updated route add

--- index.js
+++ index.js
@@ -4,13 +4,20 @@ const express = require("express");
 const app = express();
 const port = 8080;
 const path = require("path");
+const methodOverride = require("method-override");
+
+app.use(methodOverride("_method"));
+app.use(express.urlencoded({extended: true}));
 
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "/views"));
 
 
 const connection = mysql.createConnection({
-    host: "localhost", user: "root", password: "root", database: "delta_app"
+    host: "localhost",
+    user: "root",
+    password: "root",
+    database: "delta_app"
 });
 
 
@@ -79,6 +86,59 @@ app.get("/user", (req, res) => {
 
 });
 
+// Edit Route
+// TODO :: /user/:id/edit
+app.get("/user/:id/edit", (req, res) => {
+    let {id} = req.params;
+    let query = `SELECT *
+                 FROM user
+                 WHERE id = '${id}'`;
+    try {
+        connection.query(query, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            console.log(result);
+            res.render('edit.ejs', {user});
+        })
+    } catch (err) {
+        console.log(err);
+        res.send(`Some error in DB ${err}`);
+    }
+});
+
+// UPDATE(DB) Route
+app.patch("/user/:id", (req, res) => {
+    let {id} = req.params;
+    let {password: formPass, username: newUsername} = req.body;
+    // SQL Query
+    let q = `SELECT *
+             FROM user
+             WHERE id = '${id}'`;
+    try {
+        connection.query(q, (err, result) => {
+            if (err) throw err;
+            let user = result[0];
+            if (formPass !== user.password) {
+                res.send("WRONG password");
+            } else {
+                let q2 = `UPDATE user
+                          SET username = '${newUsername}'
+                          WHERE id = '${id}'`;
+                connection.query(q2, (err, result) => {
+                    if (err) throw err;
+                    res.redirect("/user");
+                });
+            }
+        })
+    } catch (err) {
+        console.log(err);
+        res.send(`Some error in DB ${err}`);
+    }
+
+});
+
 app.listen(port, (req, res) => {
+    console.log(req);
+    console.log(res);
     console.log(`Listening port is ${port}`);
 })
